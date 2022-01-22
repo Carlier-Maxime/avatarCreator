caa931fdcc727e880b03aefdd98914618af4ac27
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-01-22 10:33:15 +0100

    init list color and pas color

--- Skin.cpp
+++ Skin.cpp
@@ -1,1 +1,18 @@
 #include "Skin.h"
+
+const QList<QColor> Skin::listHumanColors =
+{
+	QColor(227,195,182),
+	QColor(227,163,125),
+	QColor(92,39,31),
+	QColor(19,1,1)
+};
+const QList<QColor> Skin::listAlienColors =
+{
+	QColor(157,62,249),
+	QColor(18,32,128),
+	QColor(0,117,105),
+	QColor(0,183,7),
+	QColor(141,52,52)
+};
+const int Skin::PAS_COLOR = 100;
